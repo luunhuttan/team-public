95307de839b01282f77fdeff3e4c43f85a6b6302
luunhuttan <luunhuttan1202@gmail.com>
2025-05-20 20:41:04 +0700

    nop code

---
+++ BAI1_28TECH.cpp
@@ -0,0 +1,13 @@
+﻿// BAI1_28TECH.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+    cout << 1ll*x * x * x + 3ll* x * x+x + 1;
+
+}
+
