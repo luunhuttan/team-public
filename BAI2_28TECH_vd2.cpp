dad38cc798779be385dc72b205d2830226223089
luunhuttan <luunhuttan1202@gmail.com>
2025-05-20 20:49:38 +0700

    nop code bai 2

---
+++ BAI2_28TECH_vd2.cpp
@@ -0,0 +1,14 @@
+﻿// BAI2_28TECH_vd2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c,S;
+    cout << "Nhap ba so a,b,c: "; cin >> a >> b >> c;
+    S = 1ll*a*(b + c) + 1ll*b*(a + c);
+    cout << S;
+
+}
+
