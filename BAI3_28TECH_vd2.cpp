d224bf03a938c192c307a50053bcc3f739f8b3ce
luunhuttan <91323064+luunhuttan@users.noreply.github.com>
2025-05-21 13:33:03 +0700

    Add files via upload

---
+++ BAI3_28TECH_vd2.cpp
@@ -0,0 +1,22 @@
+﻿// BAI3_28TECH_vd2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+	long long  a, b;
+	cin >> a >> b;
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	if (b != 0) {
+		cout << fixed << setprecision(4) << double(a / b);
+	}
+	else {
+		cout << "INVALID";
+	}
+	return 0;
+}
+
